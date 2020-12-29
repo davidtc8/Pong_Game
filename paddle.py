fa96c5c00b3dc47c906686f4733787bf79bf62d0
davidtc8 <davidtorresc8@gmail.com>
2020-12-28 23:00:23 -0600

    Paddle Class for the Game

--- paddle.py
+++ paddle.py
@@ -5,24 +5,21 @@ move_distance = 4
 up = 90
 down = 270
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self, position):
-        self.paddle = self.create_paddle(position)
-
-    def create_paddle(self, position):
-        paddle = Turtle(shape='square')
-        paddle.color('white')
-        paddle.penup()
-        paddle.shapesize(stretch_wid = 5, stretch_len = 1)
-        paddle.goto(position)
-        return paddle
+        super().__init__()
+        self.shape('square')
+        self.color('white')
+        self.shapesize (stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
     def down(self):
-        x_stays_the_same = self.paddle.xcor ()
-        new_y = self.paddle.ycor () - 20
-        self.paddle.goto (x_stays_the_same, new_y)
+        x_stays_the_same = self.xcor ()
+        new_y = self.ycor () - 20
+        self.goto (x_stays_the_same, new_y)
 
     def up(self):
-        x_stays_the_same = self.paddle.xcor()
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(x_stays_the_same, new_y)
+        x_stays_the_same = self.xcor()
+        new_y = self.ycor() + 20
+        self.goto(x_stays_the_same, new_y)
